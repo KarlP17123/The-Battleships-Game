1edb46262930cb8c93ffd118dc1f7c108927539f
KarlP17123 <karl.park@telia.com>
2022-09-28 20:23:09 +0000

    Add the diffrends numbers to hit your opponents

--- run.py
+++ run.py
@@ -1,3 +1,43 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+board = []
+
+for x in range(5):
+    board.append(["0"] * 4)
+
+def print_board(board):
+    for row in board:
+        print((" ").join(row))
+
+print("Let's Play Some Battleship Game!")
+print_board(board)
+
+def random_row(board):
+    return randint(0, len(board) - 1)
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
+
+ship_row = random_row(board)
+ship_col = random_col(board)
+
+for turn in range(9):
+    print ("Turn"), turn
+    guess_row = int(input("Guess Row:"))
+    guess_col = int(
+        input("Guess Col:"))
+
+    if guess_row == ship_row and guess_col == ship_col:
+        print("Congratulations! You sunk my battleship!")
+        break
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print("Oh You Missed It Completely.")
+        elif(board[guess_row][guess_col] == "X"):
+            print("You Have Already Selected It.")
+        else:
+            print("You missed my ship")
+            board[guess_row][guess_col] = "X"
+    if turn == 8:
+        print("Game Over")
+    turn =+ 1
+    print_board(board)
